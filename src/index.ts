8c156a03b0048c96d8465545c9a2dfa4a553b817
Hans Brende <hansbrende@apache.org>
2024-11-23 19:11:14 -0600

    add mod function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export default function add(x: number, y: number) {
-    return x + y;
-}
+
+/*
+    Gives the remainder of the division of n by d, with an optional offset.
+    Equivalent to https://reference.wolfram.com/language/ref/Mod.html
+ */
+export const mod = (n: number, d: number, offset = 0) =>
+    ((n - offset) % d + d) % d + offset;
